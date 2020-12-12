7914f6b1a1fdea374d56fb271dfedc9ab18a7370
Billy F <willfira717@gmail.com>
2020-12-12 16:54:35 -0500

    added seed data & migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.create!(name: "Lockhead", species: "Dragon", temperment: "Impish")
+Pet.create!(name: "Bella", species: "Cat", temperment: "Adventurous")
+Pet.create!(name: "Delilah", species: "Dog", temperment: "Friendly")
+Pet.create!(name: "Hedwig", species: "Owl", temperment: "Stoic")
+Pet.create!(name: "Bruin", species: "Bear", temperment: "Powerful")
+Pet.create!(name: "Horton", species: "Elephant", temperment: "Shy")
